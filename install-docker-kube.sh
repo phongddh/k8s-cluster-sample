d98d3bd1bb97122021dcfaa23ff30fbac618cc5d
phongddh <phong.daodh@gmail.com>
2024-05-22 13:03:42 +0700

    Update Vagranfile and script

--- install-docker-kube.sh
+++ install-docker-kube.sh
@@ -5,17 +5,14 @@ export DEBIAN_FRONTEND=noninteractive
 
 # 1. Install Containerd
 # Install Containerd Runtime
-sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
-
-# Enable docker repository
+sudo apt-get update -y
+sudo apt-get install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
-sudo apt update -y
-sudo apt install -y containerd.io
+sudo apt-get update -y
+sudo apt-get install -y containerd.io
 
 # Configure containerd so that it starts using systemd as cgroup.
-containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
 sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 sudo systemctl restart containerd
 sudo systemctl enable containerd
@@ -35,13 +32,11 @@ net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
 EOF
-
-# Apply sysctl params without reboot
 sudo sysctl --system
 
 # Add apt repo file for Kubernetes
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 
 # Install Kubernetes Tools
@@ -57,6 +52,5 @@ sudo swapoff -a
 # Then use the sed command below to make the necessary adjustments to the /etc/fstab file:
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
 
+# Initialize Kubernetes control plane
 sudo kubeadm init --control-plane-endpoint=k8s.master.pong
-
-# Configure NetworkManager before attempting to use Calico networking

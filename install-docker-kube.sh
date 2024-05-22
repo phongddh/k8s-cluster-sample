2cb15be8f6dd58c7aa39117347c37c794af09c8e
phongddh <phong.daodh@gmail.com>
2024-05-22 12:43:06 +0700

    Update Vagranfile and script

--- install-docker-kube.sh
+++ install-docker-kube.sh
@@ -3,26 +3,22 @@
 # Đặt DEBIAN_FRONTEND thành noninteractive để tránh lỗi dpkg-preconfigure
 export DEBIAN_FRONTEND=noninteractive
 
-# 1. Install Docker
-# Add Docker's official GPG key:
-sudo apt-get update -y
-sudo apt-get install -y ca-certificates curl
-sudo install -m 0755 -d /etc/apt/keyrings
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
-sudo chmod a+r /etc/apt/keyrings/docker.asc
+# 1. Install Containerd
+# Install Containerd Runtime
+sudo apt install -y curl gnupg2 software-properties-common apt-transport-https ca-certificates
 
-# Add the repository to Apt sources:
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
-  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y
-# Install the Docker packages.
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-sudo usermod -aG docker $(whoami)
-sudo systemctl enable docker
+# Enable docker repository
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/docker.gpg
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+sudo apt update -y
+sudo apt install -y containerd.io
+
+# Configure containerd so that it starts using systemd as cgroup.
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
 sudo systemctl enable containerd
-sudo systemctl restart docker
 
 # 2. Install Kubernetes
 # Set up the IPV4 bridge on all nodes
@@ -44,18 +40,12 @@ EOF
 sudo sysctl --system
 
 # Add apt repo file for Kubernetes
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes.gpg
-# Add Software Repositories
-echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/kubernetes.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-# Ensure all packages are up to date:
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update -y
 
 # Install Kubernetes Tools
 sudo apt-get install -y kubeadm kubelet kubectl
-sudo systemctl enable kubelet
-sudo systemctl restart containerd
-sudo systemctl restart kubelet
 
 # Mark the packages as held back to prevent automatic installation, upgrade, or removal:
 sudo apt-mark hold kubeadm kubelet kubectl
@@ -66,5 +56,7 @@ sudo swapoff -a
 
 # Then use the sed command below to make the necessary adjustments to the /etc/fstab file:
 sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+
+sudo kubeadm init --control-plane-endpoint=k8s.master.pong
 
 # Configure NetworkManager before attempting to use Calico networking
